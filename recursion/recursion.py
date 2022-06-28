e1adea41dc19161eb2ad6c72be1969337834fb08
Christian Rua <christian.rua@gmail.com>
2022-06-28 10:18:15 -0500

    adding fibonacci recursive function

--- recursion/recursion.py
+++ recursion/recursion.py
@@ -8,4 +8,20 @@ def inception(counter):
     counter += 1
     return inception(counter)
 
-print(inception(counter))
+#print(inception(counter))
+
+"""
+Given a number N return the index value of the Fibonacci sequence, where the sequence is:
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
+
+The pattern of the secuence is that each value is the sum of the 2 previous values, that means that 
+for N=5 -> 2 + 3
+"""
+
+def fibonacciIterativeRecursive(index, num1=0,num2=1):
+    if index == 1:
+        return num2
+    return fibonacciIterativeRecursive(index-1, num1=num2, num2=num1+num2)
+
+print(fibonacciIterativeRecursive(8))        
